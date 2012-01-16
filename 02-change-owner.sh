db16ebf03a9daa87f8c31774cf9960759f9d6429
Troy Will <troydwill@gmail.com>
2012-01-16 09:26:20 -0800

    modified:   02-change-owner.sh

--- 02-change-owner.sh
+++ 02-change-owner.sh
@@ -2,4 +2,5 @@
 PATH_TO_FOSWIKI='/srv/http/foswiki'
 USER='http'
 GROUP='http'
-chown --recursive --verbose ${USER}:${GROUP} ${PATH_TO_FOSWIKI}
+chown --recursive ${USER}:${GROUP} ${PATH_TO_FOSWIKI}
+
