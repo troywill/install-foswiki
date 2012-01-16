1e2f3db0264004c6a47a648eb77d506c87913f74
Troy Will <troydwill@gmail.com>
2012-01-16 13:39:34 -0800

    modified:   08-adjust-perl-path.sh

--- 08-adjust-perl-path.sh
+++ 08-adjust-perl-path.sh
@@ -5,11 +5,4 @@ FOSWIKI_PATH='/srv/http/foswiki'
 FOSWIKI_BIN_PATH="${FOSWIKI_PATH}/bin"
 FOSWIKI_TOOLS_PATH="${FOSWIKI_PATH}/tools"
 
-cd ${FOSWIKI_BIN_PATH} && && ${PERL} ../tools/rewriteshebang.pl
-
-# At the prompt, enter the full path to the perl executable, including
-# the full filename of the executable. You will be prompted twice for
-# this information in order to confirm it.
-
-cd ${FOSWIKI_TOOLS_PATH} && ${PERL} rewriteshebang.pl
-
+cd ${FOSWIKI_BIN_PATH} && ${PERL} -I ${FOSWIKI_PATH}/lib ../tools/rewriteshebang.pl
