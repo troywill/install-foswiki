3cde6e0e2245c02d80b2de0854d6e7f4573a6baf
Troy Will <troydwill@gmail.com>
2012-01-16 10:22:51 -0800

    new file:   05-config.sh

---
+++ 05-config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+EDITOR='emacs'
+cp -v /srv/http/foswiki/bin/LocalLib.cfg.txt /srv/http/foswiki/bin/LocalLib.cfg
+${EDITOR} /srv/http/foswiki/bin/LocalLib.cfg
+
+
+
---
+++ 07-sendmail.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# http://matt-linux-log.blogspot.com/2010/09/set-up-sendmail-in-arch-linux.html
+CONFIG='/etc/mail.rc'
+TEMPFILE='/tmp/delete_me'
+EDITOR='emacs'
+
+
+pacman --sync mailx-heirloom
+
+cat > ${TEMPFILE} <<EOF
+set sendmail="/usr/bin/mailx"
+set smtp=smtp.gmail.com:587
+set smtp-use-starttls
+set ssl-verify=ignore
+set ssl-auth=login
+set smtp-auth-user=login@host
+set smtp-auth-password=PASSWORD
+EOF
+
+${EDITOR} ${CONFIG} ${TEMPFILE}
