fac58230699612338f97c25b9016d6142fbcfd8f
Troy Will <troydwill@gmail.com>
2012-01-21 20:35:47 -0800

    10-config.sh -> 10-config-LocalLib.sh

--- 12-set-datadir-password.sh
+++ 12-set-datadir-password.sh
@@ -4,6 +4,8 @@ USERNAME='troy'
 PASSWORD_FILE='.htpasswd'
 GROUP='http'
 
+echo "Setting password for ${USERNAME} in {$DATA_DIR}"
+
 cd ${DATA_DIR} && sudo htpasswd -c ${PASSWORD_FILE} ${USERNAME} && \
     sudo chgrp --verbose ${GROUP} ${PASSWORD_FILE} && \
     sudo chmod --verbose g+w ${PASSWORD_FILE}
